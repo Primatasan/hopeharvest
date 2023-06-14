3554fa3214c3f3b91202a84794161f7ed41e9f27
Carlos Sanmartin <contato@carlosmsdesign.com.br>
2023-06-14 18:41:31 -0300

    Update index.js

--- src/components/Faq/Faqitem/index.js
+++ src/components/Faq/Faqitem/index.js
@@ -11,8 +11,8 @@ import './FaqItem.css'
 export default function FaqItem(props){
   return(
     <AccordionItem borderTopWidth={'0'}>
-      <AccordionButton  _expanded={{ bg: '#F17456', color: '#FCFCFC'}} className='faq_title'>
-        <div flex='1' textAlign='left' borderTopColor={'white'}>
+      <AccordionButton  _expanded={{ bg: '#F17456', color: '#FCFCFC'}}>
+        <div flex='1' textAlign='left' borderTopColor={'white'} className='faq_title'>
           <h3>{props.title}</h3>
         </div>
         <AccordionIcon />
